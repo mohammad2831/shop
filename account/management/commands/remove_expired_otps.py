15c9cf92b7a0c659b4599aa0019840337e479feb
Mohammad_2831 <101050845+mohammad2831@users.noreply.github.com>
2024-05-14 00:15:00 +0330

    Add files via upload

---
+++ account/management/commands/remove_expired_otps.py
@@ -0,0 +1,15 @@
+from typing import Any
+from django.core.management.base import BaseCommand
+from account.models import OtpCode
+from datetime import datetime, timedelta
+
+
+
+
+class Command(BaseCommand):
+    help = 'remove all expired codes .'
+
+    def handle(self, *args, **options):
+        expired_time = datetime.now() - timedelta(minutes=2)
+        OtpCode.objects.filter(created__lt=expired_time).delete()
+        self.stdout.write('all expired otp codes removed ')
